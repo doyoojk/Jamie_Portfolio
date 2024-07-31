a1b8d16f98a5372f7e98a90bcf1fc5bd9e92422b
Jamie Kim <77351173+doyoojk@users.noreply.github.com>
2024-07-31 15:18:24 -0700

    added svg to skills page

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ window.addEventListener('scroll', () => {
     // Calculate intermediate color between #19141F and #4D637C
     const startColor = { r: 25, g: 20, b: 31 };
     const endColor = { r: 77, g: 99, b: 124 };
+    // const endColor = { r:96, g:196, b:255};
     const newColor = {
         r: Math.round(startColor.r + (endColor.r - startColor.r) * ratio),
         g: Math.round(startColor.g + (endColor.g - startColor.g) * ratio),
